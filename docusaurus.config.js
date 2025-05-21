cfbd7239dcb662169d54765f4575ca7bc727ab9d
nyx <nyx@artyfetes.com>
2025-05-21 11:14:20 +0200

    update json

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,20 +10,23 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'NyxoWiki',
+  tagline: 'Quelle galère docusaurus',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://nyxluna457/github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Nyxosaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'NyxLuna457', // Usually your GitHub org/user name.
+  projectName: 'Nyxosaurus', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -148,4 +151,5 @@ const config = {
     }),
 };
 
-export default config;
+module.exports = config;
+
