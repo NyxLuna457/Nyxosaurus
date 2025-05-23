7a89ac01e2b416d5637f0019c3e81cfed0f99612
nyx <nyx@artyfetes.com>
2025-05-23 09:46:18 +0200

    test redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,24 +6,17 @@
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'NyxoWiki',
   tagline: 'Quelle galère docusaurus',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://nyxluna457.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Nyxosaurus/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'NyxLuna457', // Usually your GitHub org/user name.
-  projectName: 'Nyxosaurus', // Usually your repo name.
+  organizationName: 'NyxLuna457',
+  projectName: 'Nyxosaurus',
 
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
@@ -31,9 +24,6 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -46,8 +36,6 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
@@ -57,11 +45,8 @@ const config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -73,10 +58,23 @@ const config = {
     ],
   ],
 
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            from: '/',
+            to: '/docs/category/projet-application-python',
+          },
+        ],
+      },
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'My Site',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import {Redirect} from '@docusaurus/router';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Redirect to={`${siteConfig.baseUrl}docs/category/projet-application-python`} />
-  );
-}
 
